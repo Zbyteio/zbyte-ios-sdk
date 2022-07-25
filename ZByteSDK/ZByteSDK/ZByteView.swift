06030cf7a759a4b2a599885c02f11c11fca90e92
HardikDwata <hardik@dwatatech.com>
2022-07-26 02:30:06 +0530

    Added extension to convert String to Json format

--- ZByteSDK/ZByteSDK/ZByteView.swift
+++ ZByteSDK/ZByteSDK/ZByteView.swift
@@ -283,3 +283,10 @@ extension WKWebView {
     }
     
 }
+//Extension for JSON
+extension String {
+    func toJSON() -> Any? {
+        guard let data = self.data(using: .utf8, allowLossyConversion: false) else { return nil }
+        return try? JSONSerialization.jsonObject(with: data, options: .mutableContainers)
+    }
+}
