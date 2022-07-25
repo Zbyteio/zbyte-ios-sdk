cb1a8bae9470956a3305c05446101289d34d76ed
HardikDwata <hardik@dwatatech.com>
2022-07-26 02:23:47 +0530

    Added extension to fetch cookies and maintain user session

--- ZByteSDK/ZByteSDK/ZByteView.swift
+++ ZByteSDK/ZByteSDK/ZByteView.swift
@@ -135,3 +135,114 @@ public class ZByteView:UIView,WKNavigationDelegate
     }
     
 }
+
+extension WKWebView {
+
+    private var httpCookieStore: WKHTTPCookieStore  { return WKWebsiteDataStore.default().httpCookieStore }
+
+    func getCookies(for domain: String? = nil, completion: @escaping ([String : Any])->())  {
+        var cookieDict = [String : AnyObject]()
+        httpCookieStore.getAllCookies { cookies in
+            for cookie in cookies {
+                if let domain = domain {
+                    if cookie.domain.contains(domain) {
+                        cookieDict[cookie.name] = cookie.properties as AnyObject?
+                    }
+                } else {
+                    cookieDict[cookie.name] = cookie.properties as AnyObject?
+                }
+            }
+            completion(cookieDict)
+        }
+    }
+}
+
+extension ZByteView {
+    
+    public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        
+        
+        webView.loadDiskCookies(for: URL(string: ZByteViewConfiguration.urlStr)!.host!){
+        }
+        
+        if navigationAction.targetFrame == nil, let url = navigationAction.request.url {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
+        decisionHandler(WKNavigationActionPolicy.allow)
+    }
+
+    
+    public func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        //write cookie for current domain
+        webView.writeDiskCookies(for: URL(string: ZByteViewConfiguration.urlStr)!.host!){
+            decisionHandler(.allow)
+        }
+    }
+}
+
+extension WKWebView {
+    
+    enum PrefKey {
+        static let cookie = "cookies"
+    }
+    
+    func writeDiskCookies(for domain: String, completion: @escaping () -> ()) {
+        fetchInMemoryCookies(for: domain) { data in
+            print("write data", data)
+            UserDefaults.standard.setValue(data, forKey: PrefKey.cookie + domain)
+            completion();
+        }
+    }
+    
+    
+    func loadDiskCookies(for domain: String, completion: @escaping () -> ()) {
+        if let diskCookie = UserDefaults.standard.dictionary(forKey: (PrefKey.cookie + domain)){
+            fetchInMemoryCookies(for: domain) { freshCookie in
+                
+                let mergedCookie = diskCookie.merging(freshCookie) { (_, new) in new }
+                
+                for (cookieName, cookieConfig) in mergedCookie {
+                    let cookie = cookieConfig as! Dictionary<String, Any>
+                    
+                    var expire : Any? = nil
+                    
+                    if let expireTime = cookie["Expires"] as? Double{
+                        expire = Date(timeIntervalSinceNow: expireTime)
+                    }
+                    
+                    let newCookie = HTTPCookie(properties: [
+                        .domain: cookie["Domain"] as Any,
+                        .path: cookie["Path"] as Any,
+                        .name: cookie["Name"] as Any,
+                        .value: cookie["Value"] as Any,
+                        .secure: cookie["Secure"] as Any,
+                        .expires: expire as Any
+                    ])
+                    
+                    self.configuration.websiteDataStore.httpCookieStore.setCookie(newCookie!)
+                }
+                
+                completion()
+            }
+            
+        }
+        else{
+            completion()
+        }
+    }
+    
+    func fetchInMemoryCookies(for domain: String, completion: @escaping ([String: Any]) -> ()) {
+        var cookieDict = [String: AnyObject]()
+        WKWebsiteDataStore.default().httpCookieStore.getAllCookies { (cookies) in
+            for cookie in cookies {
+                if cookie.domain.contains(domain) {
+                    cookieDict[cookie.name] = cookie.properties as AnyObject?
+                }
+            }
+            completion(cookieDict)
+        }
+    }
+    
+}
